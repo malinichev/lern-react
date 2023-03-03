5a55df4a0f13b006020f50343a68767fc3ccaa98
malini4 <malinichev_s@mail.ru>
2023-03-03 14:34:35 +0800

    Add middleware to redux

--- src/app/providers/StoreProvider/config/createReduxStore.ts
+++ src/app/providers/StoreProvider/config/createReduxStore.ts
@@ -1,18 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import {
+    Action, configureStore, Dispatch, isAnyOf, isFulfilled, ReducersMapObject,
+} from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions, userReducer } from 'entities/User';
+import { loginByUserName } from 'features/AuthByUsername/model/services/loginByUserName/loginByUserName';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 import { counterReducer } from 'entities/Counter';
-import { userReducer } from 'entities/User';
 import { loginReducer } from 'features/AuthByUsername';
-import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
-    return configureStore<StateSchema>({
-        reducer: {
-            counter: counterReducer,
-            user: userReducer,
-            login: loginReducer,
-        },
+interface Store {
+    dispatch: Dispatch;
+    getState: () => StateSchema;
+}
+
+const isLoggedIn = isFulfilled(loginByUserName);
+const isLoggedOut = isAnyOf(userActions.logout);
+
+const authMiddleware = (store: Store) => (next: (action: Action) => void) => (action: Action): void => {
+    if (isLoggedIn(action)) {
+        localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(action.payload));
+    }
+
+    if (isLoggedOut(action)) {
+        localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+    }
+
+    next(action);
+};
+
+export function createReduxStore(initialState: StateSchema) {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        counter: counterReducer,
+        user: userReducer,
+        login: loginReducer,
+    };
+
+    return configureStore({
+        reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware),
     });
 }
--- src/features/AuthByUsername/model/services/loginByUserName/loginByUserName.ts
+++ src/features/AuthByUsername/model/services/loginByUserName/loginByUserName.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { User, userActions } from 'entities/User';
-import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 interface LoginByUserNameProps {
     username:string
@@ -16,7 +15,7 @@ export const loginByUserName = createAsyncThunk<User, LoginByUserNameProps, {rej
             if (!response.data) {
                 throw new Error();
             }
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+
             thunkAPI.dispatch(userActions.setAuthData(response.data));
 
             return response.data;
