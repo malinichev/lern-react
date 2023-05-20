aadc3dba123922960796b0d812092e215335815a
malini4 <malinichev_s@mail.ru>
2023-05-20 14:37:47 +0800

    Fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSideBarItems.ts
@@ -0,0 +1,42 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getUserAuthData } from 'entities/User';
+import { SidebarItemType } from 'widgets/Sidebar/model/types/sidebar';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
+import ArticlesIcon from 'shared/assets/icons/article-20-20.svg';
+
+export const getSideBarItems = createSelector(getUserAuthData, (authData) => {
+    const sidebarItems: SidebarItemType[] = [
+        {
+            path: RoutePath.main,
+            Icon: MainIcon,
+            text: 'Главная',
+        },
+        {
+            path: RoutePath.about,
+            Icon: AboutIcon,
+            text: 'О сайте',
+        },
+    ];
+
+    if (authData) {
+        sidebarItems.push(
+            {
+                path: RoutePath.profile + authData.id,
+                Icon: ProfileIcon,
+                text: 'Профиль',
+                authOnly: true,
+            },
+            {
+                path: RoutePath.articles,
+                Icon: ArticlesIcon,
+                text: 'Статьи',
+                authOnly: true,
+            },
+        );
+    }
+
+    return sidebarItems;
+});
