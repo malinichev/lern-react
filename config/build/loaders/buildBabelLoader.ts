06d9b0723ab7e876b02368f7139c28b2a9578f2b
malini4 <malinichev_s@mail.ru>
2023-08-09 15:23:10 +0800

    Add babel Remove Props Plugin

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,4 +1,5 @@
 import { BuildOptions } from '../types/config';
+import babelRemovePropsPlugin from '../../babel/babelRemovePropsPlugin';
 
 interface BuildBabelLoaderProps extends BuildOptions {
     isTsx?:boolean
@@ -22,6 +23,11 @@ export function buildBabelLoader({ isDev, isTsx }:BuildBabelLoaderProps) {
                     ],
                     ['@babel/plugin-transform-typescript', { isTsx }],
                     '@babel/plugin-transform-runtime',
+                    isTsx && [
+                        babelRemovePropsPlugin(), {
+                            props: ['data-testid'],
+                        },
+                    ],
                     isDev && require.resolve('react-refresh/babel'),
                 ].filter(Boolean),
             },
