deea624f74bc401173dfe87b0e6bdeef1bc182ee
smalinichev <smalinichev@samokat.ru>
2024-02-14 15:43:19 +0800

    Add https to json-server

--- json-server/index.js
+++ json-server/index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
 
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 const server = jsonServer.create();
 
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
@@ -21,7 +26,9 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
         const { users = [] } = db;
 
         const userFromBd = users.find(
@@ -51,7 +58,9 @@ server.use((req, res, next) => {
 
 server.use(router);
 
+const httpsServer = https.createServer(options, server);
+
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
+httpsServer.listen(443, () => {
+    console.log('server is running on 443 port');
 });
