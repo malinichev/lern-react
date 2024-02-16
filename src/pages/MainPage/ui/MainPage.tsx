978dd1f81124730f3fd20c03192cb02484861a7d
smalinichev <smalinichev@samokat.ru>
2024-02-16 21:54:03 +0800

    Add toggle flags and auto remove script

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,15 +2,29 @@ import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
 import { Counter } from '@/entities/Counter';
+import { toggleFeatures } from '@/shared/lib/features';
 
 const MainPage = memo(() => {
     const { t } = useTranslation('main');
+
+    const counter = toggleFeatures({
+        name: 'isCounterEnabled',
+        on: () => <Counter />,
+        off: () => null,
+    });
+
+    toggleFeatures({
+        name: 'isCounterEnabled',
+        on: () => console.log('ON blablabla'),
+        off: () => console.log('OFF blablabla'),
+    });
+
     return (
         <Page data-testid="MainPage">
             <div>33333</div>
             <div>3323234233</div>
             {t('Главная страница')}
-            <Counter />
+            {counter}
         </Page>
     );
 });
