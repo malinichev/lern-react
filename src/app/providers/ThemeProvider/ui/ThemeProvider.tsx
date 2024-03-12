ab4b6e63cbb247cce4ab012611b871d4b5c6d66a
smalinichev <smalinichev@samokat.ru>
2024-03-12 14:21:37 +0800

    Fix skeleton size, redesign scroll

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -24,6 +24,10 @@ const ThemeProvider = (props: ThemeProviderProps) => {
         }
     }, [defaultTheme, isThemeInit]);
 
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
     const defaultProps = useMemo(
         () => ({
             theme,
